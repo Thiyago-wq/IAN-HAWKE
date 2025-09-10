b32634b3cbe52a9c5058755492724bddd9c384b6
Thiyago-wq <thiagopaiva2201@gmail.com>
2025-09-10 08:35:30 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
+const display = document.getElementById("display");
+
+function append(char) {
+  display.value += char;
+}
+
+function clearDisplay() {
+  display.value = '';
+}
+
+function sqrt() {
+  try {
+    display.value = Math.sqrt(eval(display.value));
+  } catch {
+    display.value = 'Erro';
+  }
+}
+
+function power() {
+  display.value += '**';
+}
+
+function calculate() {
+  try {
+    display.value = eval(display.value);
+  } catch {
+    display.value = 'Erro';
+  }
+}
+
+// Abas
+function showTab(id) {
+  const tabs = document.querySelectorAll('.tab');
+  tabs.forEach(tab => tab.classList.remove('active'));
+
+  document.getElementById(id).classList.add('active');
+}
 
